e71645241b2d2474080cff5afe659a8cfe5d3e66
Artiom Gutsu <unreppio@gmail.com>
2024-06-25 01:46:41 +0300

    fix: remove obsolete components

--- frontend/components/landing/CardsCarousel.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div
-    class="w-full flex gap-5 pb-5 px-10 items-stretch justify-start snap-mandatory snap-x overflow-x-auto overflow-y-visible hide-scrollbar"
-  >
-    <slot />
-  </div>
-</template>
